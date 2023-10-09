a58788482b97a31ef78da556ae16d92f9fbd15fc
Rebeca Pastl <rebeca.pastl@gmail.com>
2023-10-09 12:53:54 -0700

    refine connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 let MONGO_DB = process.env.ATLAS_URI;
 
-mongoose.connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology:true });
+mongoose
+  .connect(MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 let connection = mongoose.connection;
-connection.on('error', console.error.bind(console, 'MongoDB connection error:'));
+connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
 
 module.exports = connection;
